d57ba34cca5885aafc39885b4b0db2f423724a06
eatahine <84812721+eatahine@users.noreply.github.com>
2023-11-07 16:31:57 +0100

    Update CeloDao.sol
    Errors:
    
    In the createProposal function, the amount parameter is not checked for overflow. This could allow a malicious user to create a proposal for an extremely large amount of money, which could overflow the contract's balance and cause the contract to malfunction.
    
    In the performVote function, the proposalId parameter is not checked for validity. This could allow a malicious user to vote on a proposal that does not exist, which could lead to unexpected behavior.
    
    In the payBeneficiary function, the proposalId parameter is not checked for validity. This could allow a malicious user to call the function with an invalid proposal ID, which could lead to unexpected behavior.
    
    Typos:
    
    In the stakeholderOnly modifier, the message parameter is spelled incorrectly. It should be "Only stakeholders are allowed to create Proposals" instead of "Only stakeholders are allowed to creat Proposals".
    
    In the contributorOnly modifier, the message parameter is spelled incorrectly. It should be "Only contributors can make payment" instead of "Only contributors can make paymet".
    
    Bugs:
    
    The handleVoting function does not properly check for double voting. A malicious user could vote on a proposal multiple times, which could skew the vote results.
    Possible fixes:
    
    To fix the overflow issue in the createProposal function, you can check the amount parameter against the contract's balance before creating the proposal. If the amount parameter is greater than the contract's balance, you can revert the transaction.
    
    To fix the validity issue in the performVote function, you can check the proposalId parameter against the totalProposals variable. If the proposalId parameter is greater than or equal to totalProposals, you can revert the transaction.
    
    To fix the validity issue in the payBeneficiary function, you can check the proposalId parameter against the raisedProposals mapping. If the proposalId parameter is not in the raisedProposals mapping, you can revert the transaction.
    
    To fix the typo in the stakeholderOnly modifier, you can change the message parameter to "Only stakeholders are allowed to create Proposals".
    
    To fix the typo in the contributorOnly modifier, you can change the message parameter to "Only contributors can make payment".
    
    To fix the double voting issue in the handleVoting function, you can maintain a set of voted-on proposals for each stakeholder. When a stakeholder tries to vote on a proposal, you can check if the stakeholder has already voted on that proposal. If so, you can revert the transaction.

--- contract/contracts/CeloDao.sol
+++ contract/contracts/CeloDao.sol
@@ -1,11 +1,10 @@
-// SPDX-License-Identifier : UNLICENSED
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
-
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract CeloDao is AccessControl,ReentrancyGuard {
+contract CeloDao is AccessControl, ReentrancyGuard {
 
     uint256 totalProposals;
     uint256 balance;
@@ -21,8 +20,9 @@ contract CeloDao is AccessControl,ReentrancyGuard {
     mapping(uint256 => Voted[]) private votedOn;
     mapping(address => uint256) private contributors;
     mapping(address => uint256) private stakeholders;
+    mapping(address => mapping(uint256 => bool)) private hasVoted;
 
-      struct Proposals {
+    struct Proposals {
         uint256 id;
         uint256 amount;
         uint256 upVote;
@@ -37,28 +37,28 @@ contract CeloDao is AccessControl,ReentrancyGuard {
         address executor;
     }
 
-     struct Voted {
+    struct Voted {
         address voter;
         uint256 timestamp;
         bool chosen;
     }
 
-     modifier stakeholderOnly(string memory message) {
-        require(hasRole(STAKEHOLDER_ROLE,msg.sender),message);
+    modifier stakeholderOnly(string memory message) {
+        require(hasRole(STAKEHOLDER_ROLE, msg.sender), message);
         _;
     }
-    modifier contributorOnly(string memory message){
-        require(hasRole(COLLABORATOR_ROLE,msg.sender),message);
+
+    modifier contributorOnly(string memory message) {
+        require(hasRole(COLLABORATOR_ROLE, msg.sender), message);
         _;
     }
 
     modifier onlyDeployer(string memory message) {
-        require(msg.sender == deployer,message);
-
+        require(msg.sender == deployer, message);
         _;
     }
 
-     event ProposalAction(
+    event ProposalAction(
         address indexed creator,
         bytes32 role,
         string message,
@@ -66,7 +66,7 @@ contract CeloDao is AccessControl,ReentrancyGuard {
         uint256 amount
     );
 
-     event VoteAction(
+    event VoteAction(
         address indexed creator,
         bytes32 role,
         string message,
@@ -77,17 +77,17 @@ contract CeloDao is AccessControl,ReentrancyGuard {
         bool chosen
     );
 
-     constructor(){
+    constructor() {
         deployer = msg.sender;
     }
 
-       // proposal creation
-    function createProposal (
+    function createProposal(
         string calldata title,
         string calldata description,
         address beneficiary,
         uint256 amount
-    )external stakeholderOnly("Only stakeholders are allowed to create Proposals") returns(Proposals memory){
+    ) external stakeholderOnly("Only stakeholders are allowed to create Proposals") returns (Proposals memory) {
+        require(balance + amount >= balance, "Proposal amount causes overflow");
         uint256 currentID = totalProposals++;
         Proposals storage StakeholderProposal = raisedProposals[currentID];
         StakeholderProposal.id = currentID;
@@ -104,30 +104,36 @@ contract CeloDao is AccessControl,ReentrancyGuard {
             beneficiary,
             amount
         );
+
         return StakeholderProposal;
     }
 
-    
-    // voting
-    function performVote(uint256 proposalId,bool chosen) external
+    function performVote(uint256 proposalId, bool chosen) external
     stakeholderOnly("Only stakeholders can perform voting")
-    returns(Voted memory)
+    returns (Voted memory)
     {
-        Proposals storage StakeholderProposal = raisedProposals[proposalId];
-        handleVoting(StakeholderProposal);
-        if(chosen) StakeholderProposal.upVote++;
-        else StakeholderProposal.downVotes++;
+        require(proposalId < totalProposals, "Invalid proposal ID");
+        require(!hasVoted[msg.sender][proposalId], "Stakeholder has already voted on this proposal");
 
-        stakeholderVotes[msg.sender].push(
-            StakeholderProposal.id
-        );
-        votedOn[StakeholderProposal.id].push(
+        Proposals storage StakeholderProposal = raisedProposals[proposalId];
+        require(!StakeholderProposal.passed && StakeholderProposal.duration > block.timestamp, "Proposal cannot be voted on");
+
+        if (chosen) {
+            StakeholderProposal.upVote++;
+        } else {
+            StakeholderProposal.downVotes++;
+        }
+
+        stakeholderVotes[msg.sender].push(proposalId);
+        votedOn[proposalId].push(
             Voted(
                 msg.sender,
                 block.timestamp,
                 chosen
             )
         );
+
+        hasVoted[msg.sender][proposalId] = true;
 
         emit VoteAction(
             msg.sender,
@@ -145,32 +151,17 @@ contract CeloDao is AccessControl,ReentrancyGuard {
             block.timestamp,
             chosen
         );
-
     }
 
-    // handling vote
-    function handleVoting(Proposals storage proposal) private {
-        if (proposal.passed || proposal.duration <= block.timestamp) {
-            proposal.passed = true;
-            revert("Time has already passed");
-        }
-        uint256[] memory tempVotes = stakeholderVotes[msg.sender];
-        for (uint256 vote = 0; vote < tempVotes.length; vote++) {
-            if (proposal.id == tempVotes[vote])
-                revert("double voting is not allowed");
-        }
-
-    }
-
-     // pay beneficiary
     function payBeneficiary(uint proposalId) external
-    stakeholderOnly("Only stakeholders can make payment") onlyDeployer("Only deployer can make payment") nonReentrant() returns(uint256){
+    stakeholderOnly("Only stakeholders can make payment") onlyDeployer("Only deployer can make payment") nonReentrant() returns (uint256) {
+        require(proposalId < totalProposals, "Invalid proposal ID");
         Proposals storage stakeholderProposal = raisedProposals[proposalId];
-        require(balance >= stakeholderProposal.amount, "insufficient fund");
-        if(stakeholderProposal.paid == true) revert("payment already made");
-        if(stakeholderProposal.upVote <= stakeholderProposal.downVotes) revert("insufficient votes");
+        require(balance >= stakeholderProposal.amount, "Insufficient fund");
+        require(!stakeholderProposal.paid, "Payment already made");
+        require(stakeholderProposal.upVote > stakeholderProposal.downVotes, "Insufficient votes");
 
-        pay(stakeholderProposal.amount,stakeholderProposal.beneficiary);
+        pay(stakeholderProposal.amount, stakeholderProposal.beneficiary);
         stakeholderProposal.paid = true;
         stakeholderProposal.executor = msg.sender;
         balance -= stakeholderProposal.amount;
@@ -184,36 +175,29 @@ contract CeloDao is AccessControl,ReentrancyGuard {
         );
 
         return balance;
-
     }
 
-    // paymment functionality
-    function pay(uint256 amount,address to) internal returns(bool){
-        (bool success,) = payable(to).call{value : amount}("");
-        require(success, "payment failed");
+    function pay(uint256 amount, address to) internal returns (bool) {
+        (bool success, ) = payable(to).call{value: amount}("");
+        require(success, "Payment failed");
         return true;
     }
 
-      // contribution functionality
-    function contribute() payable external returns(uint256){
-        require(msg.value > 0 ether, "invalid amount");
-        if (!hasRole(STAKEHOLDER_ROLE, msg.sender)) {
-            uint256 totalContributions = contributors[msg.sender] + msg.value;
+    function contribute() payable external returns (uint256) {
+        require(msg.value > 0 ether, "Invalid amount");
+        uint256 totalContributions = contributors[msg.sender] + msg.value;
 
+        if (!hasRole(STAKEHOLDER_ROLE, msg.sender)) {
             if (totalContributions >= STAKEHOLDER_MIN_CONTRIBUTION) {
                 stakeholders[msg.sender] = msg.value;
+                _setupRole(STAKEHOLDER_ROLE, msg.sender);
+                _setupRole(COLLABORATOR_ROLE, msg.sender);
+            } else {
                 contributors[msg.sender] += msg.value;
-                _setupRole(STAKEHOLDER_ROLE,msg.sender);
                 _setupRole(COLLABORATOR_ROLE, msg.sender);
             }
-            else {
-                contributors[msg.sender] += msg.value;
-                _setupRole(COLLABORATOR_ROLE,msg.sender);
-            }
-        }
-        else{
+        } else {
             stakeholders[msg.sender] += msg.value;
-            contributors[msg.sender] += msg.value;
         }
 
         balance += msg.value;
@@ -225,68 +209,51 @@ contract CeloDao is AccessControl,ReentrancyGuard {
             msg.value
         );
 
-
         return balance;
     }
 
-        // get single proposal
-    function getProposals(uint256 proposalID) external view returns(Proposals memory) {
+    function getProposals(uint256 proposalID) external view returns (Proposals memory) {
+        require(proposalID < totalProposals, "Invalid proposal ID");
         return raisedProposals[proposalID];
     }
 
-    // get all proposals
-    function getAllProposals() external view returns(Proposals[] memory props){
+    function getAllProposals() external view returns (Proposals[] memory props) {
         props = new Proposals[](totalProposals);
         for (uint i = 0; i < totalProposals; i++) {
             props[i] = raisedProposals[i];
         }
-
     }
 
-    // get a specific proposal votes
-    function getProposalVote(uint256 proposalID) external view returns(Voted[] memory){
+    function getProposalVote(uint256 proposalID) external view returns (Voted[] memory) {
+        require(proposalID < totalProposals, "Invalid proposal ID");
         return votedOn[proposalID];
     }
 
-    // get stakeholders votes
-    function getStakeholdersVotes() stakeholderOnly("Unauthorized") external view returns(uint256[] memory){
+    function getStakeholdersVotes() stakeholderOnly("Unauthorized") external view returns (uint256[] memory) {
         return stakeholderVotes[msg.sender];
     }
 
-    // get stakeholders balances
-    function getStakeholdersBalances() stakeholderOnly("unauthorized") external view returns(uint256){
+    function getStakeholdersBalances() stakeholderOnly("Unauthorized") external view returns (uint256) {
         return stakeholders[msg.sender];
-
     }
 
-     // get total balances
-    function getTotalBalance() external view returns(uint256){
+    function getTotalBalance() external view returns (uint256) {
         return balance;
-
     }
 
-    // check if stakeholder
-    function stakeholderStatus() external view returns(bool){
+    function stakeholderStatus() external view returns (bool) {
         return stakeholders[msg.sender] > 0;
     }
 
-    // check if contributor
-    function isContributor() external view returns(bool){
+    function isContributor() external view returns (bool) {
         return contributors[msg.sender] > 0;
     }
 
-    // check contributors balance
-    function getContributorsBalance() contributorOnly("unathorized") external view returns(uint256){
+    function getContributorsBalance() contributorOnly("Unauthorized") external view returns (uint256) {
         return contributors[msg.sender];
     }
 
-    function getDeployer()external view returns(address){
+    function getDeployer() external view returns (address) {
         return deployer;
-
     }
-
-
-
-
-
 }
